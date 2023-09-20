711c367d86fe3b72352cf2f35be9cfa008f139ee
Lair Lozano <Lairurbieta@gmail.com>
2023-09-19 22:06:04 -0600

    19-09-2023 LGLU

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularEjemplo_02';
+  duplicarNumero(n:number):number{
+    return n*2
+  }
+  pelicula={
+    titulo:'Jojo Rabbit',
+    fechadeLanzamiento:new Date(),
+    precio:1827
+  }
 }
